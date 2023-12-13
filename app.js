acf2e5a5a48142fca72d9d477d2c6d85d722f829
Muqtadirat <muqtadir.my@gmail.com>
2023-12-13 20:09:34 +0100

    Feat: day, min and max temp display

--- app.js
+++ app.js
@@ -6,12 +6,39 @@ const searchButton = document.getElementById("get-weather");
 const currentLocation = document.getElementById("location");
 const currentTemperature = document.getElementById("temperature");
 const weatherDescription = document.getElementById("description");
+const maxTemp = document.querySelector(".max-temp");
+const minTemp = document.querySelector(".min-temp");
+const currentDay = document.querySelector("#today");
+
+// Get current day
+let now = new Date();
+let date = now.getDate();
+let AMonth = now.getMonth();
+
+let months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let month = months[now.getMonth()];
+
+currentDay.innerHTML = `${month}, ${date}`
 
 searchButton.addEventListener("click", () => {
   const location = locationInput.value;
   console.log(location);
 
-//   location.reset();
+  //   location.reset();
 
   if (location) {
     getWeather(location);
@@ -24,9 +51,13 @@ const getWeather = (location) => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      currentLocation.textContent = data.name;
-      currentTemperature.textContent = `${Math.round(data.main.temp)}°C`;
+      currentLocation.textContent = `City: ${data.name}`;
+      currentTemperature.textContent = `Temperature: ${Math.round(
+        data.main.temp
+      )} °C`;
       weatherDescription.textContent = data.weather[0].description;
+      maxTemp.textContent = `${Math.round(data.main.temp_max)}°C`;
+      minTemp.textContent = `${Math.round(data.main.temp_min)}°C`;
 
       //   console.log(data);
     })
